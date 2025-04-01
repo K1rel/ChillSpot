715b743db765b1878e4ba3621cd615c40c69a584
dacc9 <dr23112002@gmail.com>
2025-04-01 11:02:06 +0200

    models fixed

--- backend/internal/models/review.go
+++ backend/internal/models/review.go
@@ -1,22 +1,22 @@
 package models
 
 import (
-    "github.com/google/uuid"
-    "gorm.io/gorm"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Review struct {
-    ID uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-    UserID uuid.UUID `gorm:"type:uuid;not null;index"`
-    SpotID uuid.UUID `gorm:"type:uuid;not null;index"`
-    Likes int `gorm:"default:0"`
-    Text string `gorm:"type:varchar(500); not null"` // maksimum dolzhina na recenzija da bide 500 karakteri
-    CreditedAt int64 `gorm:"autoCreateTime"`
+	ID         uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	UserID     uuid.UUID `gorm:"type:uuid;not null;index"`
+	SpotID     uuid.UUID `gorm:"type:uuid;not null;index"`
+	Likes      int       `gorm:"default:0"`
+	Text       string    `gorm:"type:varchar(500); not null"` // maksimum dolzhina na recenzija da bide 500 karakteri
+	CreditedAt int64     `gorm:"autoCreateTime"`
 }
 
 func (r *Review) BeforeCreate(tx *gorm.DB) (err error) {
-    if r.ID == uuid.Nil {
-        r.ID == uuid.New()
-    }
-    return
+	if r.ID == uuid.Nil {
+		r.ID = uuid.New()
+	}
+	return
 }
--- backend/internal/models/spot.go
+++ backend/internal/models/spot.go
@@ -1,37 +1,37 @@
 package models
 
 import (
-  "time"
+	"time"
 
-  "github.com/google/uuid"
-  "github.com/jackc/pgx/v5/pgtype"
-  "gorm.io/gorm"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
+	"gorm.io/gorm"
 )
 
 type WeatherCondition string
 
 const (
-  Sunny WeatherCondition = "sunny"
-  Rainy WeatherCondition = "rainy"
-  Cloudy WeatherCondition = "cloudy"
-  Snowy WeatherCondition = "snowy"
+	Sunny  WeatherCondition = "sunny"
+	Rainy  WeatherCondition = "rainy"
+	Cloudy WeatherCondition = "cloudy"
+	Snowy  WeatherCondition = "snowy"
 )
 
 type Spot struct {
-    ID uuid.UUID `gorm:"type:uuid;default:gen_random();primaryKey"`
-    Location pgtype.Point `gorm:"type:geometry(Point, 4326); not null"`
-    Description string `gorm:"type:text;not null"`
-    DayImage *string `gorm:"type:text"`
-    NightImage *string `gorm:"type:text"`
-    RecommendedWeather WeatherCondition `gorm:"type:varshar(20)"`
-    VisitCount uint `gorm:"default:0"`
-    // favorites_count kje se inkrementira so funkcija, namesto sekoj pat kveri da se izvrshuva
-    FavoritesCount uint `gorm:"default:0" json:"favorites_count"`
-    CreatedAt time.Time
-    UpdatedAt time.Time
+	ID                 uuid.UUID        `gorm:"type:uuid;default:gen_random();primaryKey"`
+	Location           pgtype.Point     `gorm:"type:geometry(Point, 4326); not null"`
+	Description        string           `gorm:"type:text;not null"`
+	DayImage           *string          `gorm:"type:text"`
+	NightImage         *string          `gorm:"type:text"`
+	RecommendedWeather WeatherCondition `gorm:"type:varchar(20)"`
+	VisitCount         uint             `gorm:"default:0"`
+	// favorites_count kje se inkrementira so funkcija, namesto sekoj pat kveri da se izvrshuva
+	FavoritesCount uint `gorm:"default:0" json:"favorites_count"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
 }
 
 func (s *Spot) BeforeCreate(tx *gorm.DB) (err error) {
-    s.ID = uuid.New()
-    return nil
+	s.ID = uuid.New()
+	return nil
 }
